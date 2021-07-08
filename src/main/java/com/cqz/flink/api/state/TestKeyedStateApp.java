8417bb4fa5a5d3644b2a5e12ed122cbe131c07ec
chenqizhu <chenqizhu@4399inc.com>
2021-07-08 11:25:46 +0800

    schema

--- src/main/java/com/cqz/flink/api/state/TestKeyedStateApp.java
+++ src/main/java/com/cqz/flink/api/state/TestKeyedStateApp.java
@@ -4,12 +4,12 @@ import com.cqz.flink.api.beans.SensorReading;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.util.Collector;
-import scala.Tuple3;
 
 public class TestKeyedStateApp {
     public static void main(String[] args) throws Exception {
